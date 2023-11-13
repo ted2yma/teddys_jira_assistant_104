358364720e79b8fc0a948fc1d8fd54a73a41dfec
ted2yma <teddyma@alumni.scu.edu.tw>
2023-11-13 16:43:05 +0800

    add sprint info

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,85 @@
-import { Box, Button, Flex, Switch, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Switch, Text, VStack } from "@chakra-ui/react";
+import { debounce } from "lodash";
 import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+
+interface sprintsTypes {
+  id: number,
+  sequence: number,
+  name: string,
+  state: string,
+  linkedPagesCount: number,
+  goal: string,
+  sprintVersion: string,
+  startDate: string,
+  endDate: string,
+  isoStartDate: string,
+  isoEndDate: string,
+  completeDate: string,
+  isoCompleteDate: string,
+  canUpdateSprint: boolean,
+  remoteLinks: [],
+  daysRemaining: number
+}
+
+interface progressType {
+  inProgress: { hours: number, issues: number }, done: { hours: number, issues: number }, total: { hours: number, issues: number }
+}
 
 export default function App() {
   const [isClean, setIsClean] = useState<boolean>(false);
   const [prod, setProd] = useState<string | null>(null);
   const [isJira, setIsJira] = useState<boolean>(false);
+  const [progress, setProgress] = useState<progressType | null>(null);
+  const [sprintName, setSprintName] = useState<string | null>(null);
+  // const [sprintRange, setSprintRange] = useState<{ start: string, end: string } | null>(null);
   const cache = useRef(null);
+  const debouncedfetch = useRef(debounce( async () => {
+    const params = `activeQuickFilters=3670&hideCardExtraFields=true&includeHidden=true&moduleKey=agile-mobile-board-service&onlyUseEpicsFromIssues=true`;
+    const targetUrl = `https://104corp.atlassian.net/rest/boards/latest/board/316`;
+    const response = await fetch(`${targetUrl}?${params}`);
+    const resData = await response.json();
+    
+    if (response.status === 200 && resData) {
+      const { sprints, columns } = resData;
+      const latestSprint: sprintsTypes = sprints.reduce((a: sprintsTypes, b: sprintsTypes) => {
+        return new Date(a.isoEndDate) > new Date(b.isoEndDate) ? a : b;
+      })
+      let progress: progressType = {
+        inProgress: { hours: 0, issues: 0 }, done: { hours: 0, issues: 0 }, total: { hours: 0, issues: 0 }
+      };
+      columns.map(statusBlock => {
+        const currentIssues = statusBlock.issues.filter(issue => issue.sprintIds.indexOf(latestSprint.id) > -1 && issue.isVisible);
+        currentIssues.length > 0 && currentIssues.map(issue => {
+          switch (statusBlock.name) {
+            case 'To Do':
+            case '等待重做':
+            case 'In Progress':
+              progress.inProgress.hours += Number(issue.estimation);
+              progress.inProgress.issues += 1;
+              progress.total.hours += Number(issue.estimation);
+              progress.total.issues += 1;
+              break;
+            case "等待上LAB":
+            case "LAB測試中":
+            case "等待上STG":
+            case "STG測試中":
+            case "準備上線":
+            case "上線確認":
+            case "Done":
+              progress.done.hours += Number(issue.estimation);
+              progress.done.issues += 1;
+              progress.total.hours += Number(issue.estimation);
+              progress.total.issues += 1;
+              break;
+            default: 
+              break;
+          }
+        })
+      });
+      setSprintName(latestSprint.name)
+      setProgress(progress);
+    }
+  }, 500));
 
   useEffect(() => {
     let ignore = false;
@@ -13,27 +87,22 @@ export default function App() {
       chrome.tabs.query({
         currentWindow: true,
         active: true,
-      }).then(curr => {
+      }).then((curr) => {
         if (curr[0].url.includes('https://104corp.atlassian.net/jira')) {
-          setIsJira(true);
           chrome.tabs.sendMessage(
             curr[0].id,
             {
               action: "getProjectData",
             },
             (res) => {
-              setProd(res.product)
+              if (res) setProd(res.product);
             }
           );
           chrome.storage.local.get(["isClean"]).then((res) => {
             setIsClean(res["isClean"]);
           });
-          // async function fetchApi () {
-          //   const response = await fetch('https://104corp.atlassian.net/rest/boards/latest/board/316?activeQuickFilters=3670&hideCardExtraFields=true&includeHidden=true&moduleKey=agile-mobile-board-service&onlyUseEpicsFromIssues=true');
-          //   const resData = await response.json();
-          //   return resData.data;
-          // }
-          // const response = fetchApi();
+          debouncedfetch.current();
+          setIsJira(true);
         }
       })
     }
@@ -41,68 +110,129 @@ export default function App() {
   }, []);
 
   return (
-    <Box pb="4">
-      {isJira ? (
+    <HStack sx={{
+      '>div:nth-child(2)': {
+        borderLeft: `1px solid var(--chakra-colors-gray-300)`,
+      },
+      minHeight: '250px'
+    }} spacing='0'>
+      {isJira && progress && sprintName ? (
         <>
-          <Flex {...{
-            w: '250px',
-            h: '250px',
-            justify: `center`,
-            align: `center`,
-            flexFlow: `column wrap`
+          <VStack {...{
+            w: '350px',
+            spacing: 4
           }}>
-            <Text as='h1' fontSize='xl' mb="4">全頁模式{isClean ? `開啟`: `關閉`}</Text>
-            <Switch
-              isChecked={isClean}
-              size='lg'
-              onChange={(evt: ChangeEvent<HTMLInputElement>) => {
-                chrome.storage.local.set({ isClean: false }).then(async () => {
-                  const curr = await chrome.tabs.query({
-                    currentWindow: true,
-                    active: true,
-                  });
+            <VStack w='100%'>
+              <Text {...{
+                as: 'span',
+                color: 'white',
+                bg: 'red.600',
+                borderRadius: `8px`,
+                px: 3,
+                py: 1,
+                fontWeight: 'bold'
+              }}>Current Sprint</Text>
+              <Text {...{
+                as: 'h1',
+                fontSize: 'xl',
+                fontWeight: 'bold'
+              }}>{sprintName}</Text>
+            </VStack>
+            <VStack>
+              <Text {...{
+                as: 'h1',
+                fontSize: '5xl',
+                textAlign: `center`
+              }}>{(progress.done.hours / progress.total.hours) * 100}%</Text>
+            </VStack>
+            <HStack spacing='4'>
+              <Text fontSize='md'>已完成</Text>
+              <Text {...{
+                p: 2,
+                borderRadius: '8px',
+                color: 'white',
+                bg: 'teal.600'
+              }}>
+                <Text {...{
+                  as: 'span',
+                  fontSize: 'lg',
+                  fontWeight: 'bold'
+                }}>{progress.done.hours}</Text>
+                <Text {...{
+                  as: 'span',
+                  fontSize: 'md'
+                }}> / {progress.total.hours} 小時</Text>
+              </Text>
+              <Text {...{
+                p: 2,
+                borderRadius: '8px',
+                color: 'white',
+                bg: 'blue.600'
+              }}>
+                <Text {...{
+                  as: 'span',
+                  fontSize: 'lg',
+                  fontWeight: 'bold'
+                }}>{progress.done.issues}</Text>
+                <Text {...{
+                  as: 'span',
+                  fontSize: 'md'
+                }}> / {progress.total.issues} 工項</Text>
+              </Text>
+            </HStack>
+          </VStack>
+          <VStack {...{
+            w: '200px'
+          }}>
+            <Flex {...{
+              w: '100%',
+              h: '250px',
+              justify: `center`,
+              align: `center`,
+              flexFlow: `column wrap`
+            }}>
+              <Text as='h1' fontSize='xl' mb="4">全頁模式{isClean ? `開啟`: `關閉`}</Text>
+              <Switch
+                isChecked={isClean}
+                size='lg'
+                onChange={(evt: ChangeEvent<HTMLInputElement>) => {
+                  chrome.storage.local.set({ isClean: false }).then(async () => {
+                    const curr = await chrome.tabs.query({
+                      currentWindow: true,
+                      active: true,
+                    });
 
-                  chrome.tabs.sendMessage(
-                    curr[0].id,
-                    {
-                      action: "clearJiraTop",
-                      isClean: !isClean,
-                    },
-                    (res) => {
-                      console.log(res);
-                    }
-                  );
-                  setIsClean(!isClean);
-                });
-              }}
-            />
-          </Flex>
-          <Flex justify='center' pt='4' px='4' borderTop='1px solid #ddd' flexFlow='row wrap'>
-            {prod && <Text as='h3' textAlign='center' mb='4'>{prod}</Text>}
-            <Button
-              colorScheme='teal'
-              onClick={async () => {
-                const curr = await chrome.tabs.query({
-                  currentWindow: true,
-                  active: true,
-                });
-                chrome.tabs.sendMessage(
-                  curr[0].id,
-                  {
-                    action: "getProjectData",
-                  },
-                  (res) => {
-                    console.log(res);
-                    navigator.clipboard.writeText(res.product).then(() => {
+                    chrome.tabs.sendMessage(
+                      curr[0].id,
+                      {
+                        action: "clearJiraTop",
+                        isClean: !isClean,
+                      },
+                      (res) => {
+                        console.log(res);
+                      }
+                    );
+                    setIsClean(!isClean);
+                  });
+                }}
+              />
+            </Flex>
+            {prod && (
+              <Flex w='100%' justify='center' p='4' borderTop='1px solid var(--chakra-colors-gray-300)' flexFlow='row wrap'>
+                <Text as='h3' textAlign='center' mb='4'>{prod}</Text>
+                <Button
+                  colorScheme='teal'
+                  onClick={() => {
+                    navigator.clipboard.writeText(prod).then(() => {
                       alert("copy!");
                     });
-                  }
-                );
-              }}
-            >
-              複製工單
-            </Button>
-          </Flex>
+                  }}
+                >
+                  複製工單
+                </Button>
+              </Flex>  
+            )}
+          </VStack>
         </>
       ) : (
         <Flex {...{
@@ -114,6 +244,6 @@ export default function App() {
           <Text as='h1' fontSize='xl' textAlign='center'>你沒在Jira頁面餒...</Text>
         </Flex>
       )}
-    </Box>
+    </HStack>
   );
 }
--- src/content/contentScript.ts
+++ src/content/contentScript.ts
@@ -1,41 +1,56 @@
 (() => {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "clearJiraTop") {
-      chrome.storage.local.get(["isClean"]).then((res) => {
-        clearJiraTop({ isClean: message.isClean || res.isClean });
-        sendResponse({ isClean: message.isClean || res.isClean });
-      });
-    } else if (message.action === "getProjectData") {
-      let isProductExist = document.querySelector('a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]') as HTMLElement;
+    console.log(message);
+    
+    switch (message.action) {
+      case "clearJiraTop":
+        clearJiraTop({ isClean: message.isClean });
+        sendResponse({ isClean: message.isClean });
+        return true;
 
-      if (isProductExist) {
-        let productId = isProductExist.childNodes[0].textContent;
-        let productName = document.querySelector('h1[data-testid="issue.views.issue-base.foundation.summary.heading"]');
+      case "getProjectData":
+        const product_raw = getProjectData();
+        const product = product_raw.id ? `${product_raw.id} ${product_raw.name}` : ''
+        sendResponse({ product });
+        return true;
 
-        sendResponse({ product: `${productId} ${productName.textContent}` });
-      }
-    }
-
-    return true;
-
-    function clearJiraTop({ isClean }: { isClean: boolean }) {
-      let firstEle = document.querySelector('div[data-fullscreen-id="fullscreen-board-breadcrumbs"]') as HTMLElement;
-      let nav = document.querySelector('div[data-fullscreen-id="fullscreen.page-container-v2.topnav"]') as HTMLElement;
-
-      if (firstEle && nav) {
-        let secondEle = firstEle.nextElementSibling as HTMLElement;
-        let thirdEle = secondEle.nextElementSibling as HTMLElement;
-
-        document.documentElement.style.cssText = `--topNavigationHeight: ${isClean ? 0 : 56}px; --leftSidebarWidth: 240px; --leftSidebarFlyoutWidth: 240px;`;
-        nav.style.display = isClean ? "none" : "block";
-        firstEle.style.display = isClean ? "none" : "block";
-        secondEle.style.display = isClean ? "none" : "flex";
-        thirdEle.style.marginTop = isClean ? "0" : "";
-
-        chrome.storage.local.set({
-          isClean: isClean,
-        });
-      }
+      default:
+        break;
     }
   });
 })();
+
+function getProjectData(): { id: string|undefined, name: string|undefined } {
+  let results: {id: string|undefined, name: string|undefined} = {id: undefined, name: undefined};
+  let isProductExist = document.querySelector('a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]') as HTMLElement;
+
+  if (isProductExist) {
+    let productId = isProductExist.childNodes[0].textContent;
+    let productName = document.querySelector('h1[data-testid="issue.views.issue-base.foundation.summary.heading"]');
+
+    results.id = productId;
+    results.name = productName.textContent || undefined;
+  }
+
+  return results;
+}
+
+function clearJiraTop({ isClean }: { isClean: boolean }) {
+  let firstEle = document.querySelector('div[data-fullscreen-id="fullscreen-board-breadcrumbs"]') as HTMLElement;
+  let nav = document.querySelector('div[data-fullscreen-id="fullscreen.page-container-v2.topnav"]') as HTMLElement;
+
+  if (firstEle && nav) {
+    let secondEle = firstEle.nextElementSibling as HTMLElement;
+    let thirdEle = secondEle.nextElementSibling as HTMLElement;
+
+    document.documentElement.style.cssText = `--topNavigationHeight: ${isClean ? 0 : 56}px; --leftSidebarWidth: 240px; --leftSidebarFlyoutWidth: 240px;`;
+    nav.style.display = isClean ? "none" : "block";
+    firstEle.style.display = isClean ? "none" : "block";
+    secondEle.style.display = isClean ? "none" : "flex";
+    thirdEle.style.marginTop = isClean ? "0" : "";
+
+    chrome.storage.local.set({
+      isClean: isClean,
+    });
+  }
+}
